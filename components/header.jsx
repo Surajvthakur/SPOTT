29165d92a2f48dfa807d2877978cfb2ef820ed91
Surajvthakur <surajvt21@gmail.com>
2025-11-25 16:31:02 +0530

    25-11-25/16-30

---
+++ components/header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+
+    <>
+    <nav>
+        <div>
+            {/* logo */}
+            
+            {/* Search and location -desktop only */}
+
+            {/* right side actions */}
+        </div>
+
+        {/* Mobile Search and location below header */}
+    </nav>
+
+    {/* Modals */}
+    </>
+    );
+}; 
+
+
+export default Header;
